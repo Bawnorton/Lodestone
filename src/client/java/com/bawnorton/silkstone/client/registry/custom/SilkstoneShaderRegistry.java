5e2eb7d9de57380bf46f1b78b06ed085cfedf0d8
Bawnorton <bawnorton@me.com>
2024-02-07 18:22:49 -0800

    client registry de-staticification

--- src/client/java/com/bawnorton/silkstone/client/registry/custom/SilkstoneShaderRegistry.java
+++ src/client/java/com/bawnorton/silkstone/client/registry/custom/SilkstoneShaderRegistry.java
@@ -2,6 +2,7 @@ package com.bawnorton.silkstone.client.registry.custom;
 
 import com.bawnorton.silkstone.Silkstone;
 import com.bawnorton.silkstone.client.event.RegisterShadersEvent;
+import com.bawnorton.silkstone.client.registry.SilkstoneClientRegistries;
 import com.mojang.datafixers.util.Pair;
 import net.minecraft.client.gl.ShaderProgram;
 import net.minecraft.client.render.VertexFormats;
@@ -12,26 +13,26 @@ import java.util.function.Consumer;
 public final class SilkstoneShaderRegistry {
     private static final SilkstoneShaderRegistry INSTANCE = new SilkstoneShaderRegistry();
     
-    public static ShaderHolder SILKSTONE_TEXTURE = new ShaderHolder(Silkstone.id("silkstone_texure"), VertexFormats.POSITION_COLOR_TEXTURE_LIGHT, "LumiTransparency");
-    public static ShaderHolder PARTICLE = new ShaderHolder(Silkstone.id("particle/particle"), VertexFormats.POSITION_TEXTURE_COLOR_LIGHT, "LumiTransparency");
+    public ShaderHolder SILKSTONE_TEXTURE = new ShaderHolder(Silkstone.id("silkstone_texure"), VertexFormats.POSITION_COLOR_TEXTURE_LIGHT, "LumiTransparency");
+    public ShaderHolder PARTICLE = new ShaderHolder(Silkstone.id("particle/particle"), VertexFormats.POSITION_TEXTURE_COLOR_LIGHT, "LumiTransparency");
 
-    public static ShaderHolder SCREEN_PARTICLE = new ShaderHolder(Silkstone.id("screen/screen_particle"), VertexFormats.POSITION_COLOR_TEXTURE);
-    public static ShaderHolder DISTORTED_TEXTURE = new ShaderHolder(Silkstone.id("screen/distorted_texture"), VertexFormats.POSITION_COLOR_TEXTURE, "Speed", "TimeOffset", "Intensity", "XFrequency", "YFrequency", "UVCoordinates");
+    public ShaderHolder SCREEN_PARTICLE = new ShaderHolder(Silkstone.id("screen/screen_particle"), VertexFormats.POSITION_COLOR_TEXTURE);
+    public ShaderHolder DISTORTED_TEXTURE = new ShaderHolder(Silkstone.id("screen/distorted_texture"), VertexFormats.POSITION_COLOR_TEXTURE, "Speed", "TimeOffset", "Intensity", "XFrequency", "YFrequency", "UVCoordinates");
 
-    public static ShaderHolder SCROLLING_TEXTURE = new ShaderHolder(Silkstone.id("shapes/scrolling_texture"), VertexFormats.POSITION_COLOR_TEXTURE_LIGHT, "Speed", "LumiTransparency");
-    public static ShaderHolder TRIANGLE_TEXTURE = new ShaderHolder(Silkstone.id("shapes/triangle_texture"), VertexFormats.POSITION_COLOR_TEXTURE_LIGHT, "LumiTransparency");
-    public static ShaderHolder SCROLLING_TRIANGLE_TEXTURE = new ShaderHolder(Silkstone.id("shapes/scrolling_triangle_texture"), VertexFormats.POSITION_COLOR_TEXTURE_LIGHT, "Speed", "LumiTransparency");
+    public ShaderHolder SCROLLING_TEXTURE = new ShaderHolder(Silkstone.id("shapes/scrolling_texture"), VertexFormats.POSITION_COLOR_TEXTURE_LIGHT, "Speed", "LumiTransparency");
+    public ShaderHolder TRIANGLE_TEXTURE = new ShaderHolder(Silkstone.id("shapes/triangle_texture"), VertexFormats.POSITION_COLOR_TEXTURE_LIGHT, "LumiTransparency");
+    public ShaderHolder SCROLLING_TRIANGLE_TEXTURE = new ShaderHolder(Silkstone.id("shapes/scrolling_triangle_texture"), VertexFormats.POSITION_COLOR_TEXTURE_LIGHT, "Speed", "LumiTransparency");
 
 
     public static void bootstrap() {
         RegisterShadersEvent.EVENT.register(((factory, shaders) -> {
-            registerShader(shaders, SILKSTONE_TEXTURE.createInstance(factory));
-            registerShader(shaders, PARTICLE.createInstance(factory));
-            registerShader(shaders, SCREEN_PARTICLE.createInstance(factory));
-            registerShader(shaders, DISTORTED_TEXTURE.createInstance(factory));
-            registerShader(shaders, SCROLLING_TEXTURE.createInstance(factory));
-            registerShader(shaders, TRIANGLE_TEXTURE.createInstance(factory));
-            registerShader(shaders, SCROLLING_TRIANGLE_TEXTURE.createInstance(factory));
+            registerShader(shaders, SilkstoneClientRegistries.SHADER_REGISTRY.SILKSTONE_TEXTURE.createInstance(factory));
+            registerShader(shaders, SilkstoneClientRegistries.SHADER_REGISTRY.PARTICLE.createInstance(factory));
+            registerShader(shaders, SilkstoneClientRegistries.SHADER_REGISTRY.SCREEN_PARTICLE.createInstance(factory));
+            registerShader(shaders, SilkstoneClientRegistries.SHADER_REGISTRY.DISTORTED_TEXTURE.createInstance(factory));
+            registerShader(shaders, SilkstoneClientRegistries.SHADER_REGISTRY.SCROLLING_TEXTURE.createInstance(factory));
+            registerShader(shaders, SilkstoneClientRegistries.SHADER_REGISTRY.TRIANGLE_TEXTURE.createInstance(factory));
+            registerShader(shaders, SilkstoneClientRegistries.SHADER_REGISTRY.SCROLLING_TRIANGLE_TEXTURE.createInstance(factory));
         }));
     }
 
--- src/client/java/com/bawnorton/silkstone/client/registry/custom/SilkstoneWorldEventRendererRegistry.java
+++ src/client/java/com/bawnorton/silkstone/client/registry/custom/SilkstoneWorldEventRendererRegistry.java
@@ -4,10 +4,22 @@ import java.util.HashMap;
 import java.util.Map;
 
 public final class SilkstoneWorldEventRendererRegistry {
-    public static Map<WorldEventType, WorldEventRenderer<WorldEventInstance>> RENDERERS = new HashMap<>();
+    private static final SilkstoneWorldEventRendererRegistry INSTANCE = new SilkstoneWorldEventRendererRegistry();
 
-    public static void registerRenderer(WorldEventType type, WorldEventRenderer<? extends WorldEventInstance> renderer) {
+    public Map<WorldEventType, WorldEventRenderer<WorldEventInstance>> RENDERERS = new HashMap<>();
+
+    public static void bootstrap() {
+        // no-op
+    }
+
+    public static SilkstoneWorldEventRendererRegistry get() {
+        return INSTANCE;
+    }
+
+    public void registerRenderer(WorldEventType type, WorldEventRenderer<? extends WorldEventInstance> renderer) {
         RENDERERS.put(type, renderer);
     }
 
+    private SilkstoneWorldEventRendererRegistry() {
+    }
 }
